0a219c198a8393d0a08b8b3fc3183bd4e79ea70e
Александр <alexgalimov2003@mail.ru>
2024-04-14 22:21:28 +0300

    Project template

---
+++ src/main/java/cs/vsu/ru/tpbakebudget/TpBakeBudgetApplication.java
@@ -0,0 +1,13 @@
+package cs.vsu.ru.tpbakebudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpBakeBudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpBakeBudgetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cs/vsu/ru/tpbakebudget/TpBakeBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.vsu.ru.tpbakebudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpBakeBudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
